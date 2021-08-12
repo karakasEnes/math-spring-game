ec466b453d85ee88c2859375ecbd337ec8133f9a
pyuser <enes.karakas.e@gmail.com>
2021-08-12 13:02:39 +0300

    v3 done, js important topic covered, selected radio information reached

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // Pages
-const gamePage = document.getElementById('game-page');
-const scorePage = document.getElementById('score-page');
-const splashPage = document.getElementById('splash-page');
-const countdownPage = document.getElementById('countdown-page');
+const gamePage = document.getElementById("game-page");
+const scorePage = document.getElementById("score-page");
+const splashPage = document.getElementById("splash-page");
+const countdownPage = document.getElementById("countdown-page");
 // Splash Page
-const startForm = document.getElementById('start-form');
-const radioContainers = document.querySelectorAll('.radio-container');
-const radioInputs = document.querySelectorAll('input');
-const bestScores = document.querySelectorAll('.best-score-value');
+const startForm = document.getElementById("start-form");
+const radioContainers = document.querySelectorAll(".radio-container");
+const radioInputs = document.querySelectorAll("input");
+const bestScores = document.querySelectorAll(".best-score-value");
 // Countdown Page
-const countdown = document.querySelector('.countdown');
+const countdown = document.querySelector(".countdown");
 // Game Page
-const itemContainer = document.querySelector('.item-container');
+const itemContainer = document.querySelector(".item-container");
 // Score Page
-const finalTimeEl = document.querySelector('.final-time');
-const baseTimeEl = document.querySelector('.base-time');
-const penaltyTimeEl = document.querySelector('.penalty-time');
-const playAgainBtn = document.querySelector('.play-again');
+const finalTimeEl = document.querySelector(".final-time");
+const baseTimeEl = document.querySelector(".base-time");
+const penaltyTimeEl = document.querySelector(".penalty-time");
+const playAgainBtn = document.querySelector(".play-again");
 
 // Equations
-
+let questionAmount = 0;
 let equationsArray = [];
 
 // Game Page
@@ -30,18 +30,51 @@ const wrongFormat = [];
 
 // Time
 
+function getQuestionAmount() {
+  let radioValue;
+  radioInputs.forEach((radioInput) => {
+    if (radioInput.checked) {
+      radioValue = radioInput.value;
+    }
+  });
+
+  return radioValue;
+}
+
+function selectQuestionAmount(e) {
+  e.preventDefault();
+  questionAmount = getQuestionAmount();
+  console.log(questionAmount);
+}
+
+// Event Listeners ****
+
+startForm.addEventListener("submit", selectQuestionAmount);
+
+// form selecting radio
+startForm.addEventListener("click", () => {
+  radioContainers.forEach((radioEl) => {
+    radioEl.classList.remove("selected-label");
+
+    //if input (radio) checked
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add("selected-label");
+    }
+  });
+});
+
 // Scroll
 
 // Create Correct/Incorrect Random Equations
 function createEquations() {
   // Randomly choose how many correct equations there should be
-  // const correctEquations = 
+  // const correctEquations =
   // Set amount of wrong equations
-  // const wrongEquations = 
+  // const wrongEquations =
   // Loop through, multiply random numbers up to 9, push to array
   // for (let i = 0; i < correctEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
+  //   firstNumber =
+  //   secondNumber =
   //   const equationValue = firstNumber * secondNumber;
   //   const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
   //   equationObject = { value: equation, evaluated: 'true' };
@@ -49,13 +82,13 @@ function createEquations() {
   // }
   // Loop through, mess with the equation results, push to array
   // for (let i = 0; i < wrongEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
+  //   firstNumber =
+  //   secondNumber =
   //   const equationValue = firstNumber * secondNumber;
   //   wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
   //   wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
   //   wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
-  //   const formatChoice = 
+  //   const formatChoice =
   //   const equation = wrongFormat[formatChoice];
   //   equationObject = { value: equation, evaluated: 'false' };
   //   equationsArray.push(equationObject);
